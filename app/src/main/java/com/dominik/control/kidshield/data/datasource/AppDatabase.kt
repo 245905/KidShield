5713d09d0b09bebec7df5f5b83f0f24a370966ac
Your Name <you@example.com>
2025-11-14 22:54:47 +0100

    Added room database file

---
+++ app/src/main/java/com/dominik/control/kidshield/data/datasource/AppDatabase.kt
@@ -0,0 +1,38 @@
+package com.dominik.control.kidshield.data.datasource
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.dominik.control.kidshield.data.dao.AppInfoDao
+import com.dominik.control.kidshield.data.dao.HourlyStatsDao
+import com.dominik.control.kidshield.data.dao.UsageStatsDao
+import com.dominik.control.kidshield.data.model.AppInfoEntity
+import com.dominik.control.kidshield.data.model.HourlyStatsEntity
+import com.dominik.control.kidshield.data.model.UsageStatsEntity
+import com.dominik.control.kidshield.utils.Converters
+
+@Database(entities = [AppInfoEntity::class, UsageStatsEntity::class, HourlyStatsEntity::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun appInfoDao(): AppInfoDao
+    abstract fun usageStatsDao(): UsageStatsDao
+    abstract fun hourlyStatsDao(): HourlyStatsDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "kidshield_database"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+
+}
