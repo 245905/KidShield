412229106860b830a954e80eb87423f3dd019e76
Your Name <you@example.com>
2026-02-20 21:10:53 +0100

    Added sever connection for sensor data

--- app/src/main/java/com/dominik/control/kidshield/data/remote/datasource/SigMotionRemoteDataSource.kt
+++ app/src/main/java/com/dominik/control/kidshield/data/remote/datasource/SigMotionRemoteDataSource.kt
@@ -1,19 +1,19 @@
 package com.dominik.control.kidshield.data.remote.datasource
 
-import com.dominik.control.kidshield.data.model.domain.PointEntity
+import com.dominik.control.kidshield.data.model.domain.SigMotionEntity
 import com.dominik.control.kidshield.data.model.dto.toDto
 import com.dominik.control.kidshield.data.model.dto.toEntity
-import com.dominik.control.kidshield.data.remote.api.PointApi
+import com.dominik.control.kidshield.data.remote.api.SigMotionApi
 import jakarta.inject.Inject
 
-class PointRemoteDataSource @Inject constructor(private val api: PointApi) {
+class SigMotionRemoteDataSource @Inject constructor(private val api: SigMotionApi) {
 
-    suspend fun uploadData(data: List<PointEntity>){
+    suspend fun uploadData(data: List<SigMotionEntity>){
         val req = data.map { it.toDto() }
         api.uploadData(req)
     }
 
-    suspend fun downloadData(): List<PointEntity>{
+    suspend fun downloadData(): List<SigMotionEntity>{
         val req = api.downloadData()
         return req.map { it.toEntity() }
     }
--- app/src/main/java/com/dominik/control/kidshield/data/remote/datasource/StepCountRemoteDataSource.kt
+++ app/src/main/java/com/dominik/control/kidshield/data/remote/datasource/StepCountRemoteDataSource.kt
@@ -1,19 +1,19 @@
 package com.dominik.control.kidshield.data.remote.datasource
 
-import com.dominik.control.kidshield.data.model.domain.PointEntity
+import com.dominik.control.kidshield.data.model.domain.StepCountEntity
 import com.dominik.control.kidshield.data.model.dto.toDto
 import com.dominik.control.kidshield.data.model.dto.toEntity
-import com.dominik.control.kidshield.data.remote.api.PointApi
+import com.dominik.control.kidshield.data.remote.api.StepCountApi
 import jakarta.inject.Inject
 
-class PointRemoteDataSource @Inject constructor(private val api: PointApi) {
+class StepCountRemoteDataSource @Inject constructor(private val api: StepCountApi) {
 
-    suspend fun uploadData(data: List<PointEntity>){
+    suspend fun uploadData(data: List<StepCountEntity>){
         val req = data.map { it.toDto() }
         api.uploadData(req)
     }
 
-    suspend fun downloadData(): List<PointEntity>{
+    suspend fun downloadData(): List<StepCountEntity>{
         val req = api.downloadData()
         return req.map { it.toEntity() }
     }
